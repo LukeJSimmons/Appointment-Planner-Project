392a75f9e7d1e93ba67901185542dff30c150f19
LukeDaaDuke <lordlucasjsimmons@yahoo.com>
2024-04-09 07:36:28 -0500

    Add App

---
+++ src/components/contactForm/ContactForm.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export const ContactForm = ({
+  name,
+  setName,
+  phone,
+  setPhone,
+  email,
+  setEmail,
+  handleSubmit
+}) => {
+  return (
+    <>
+      <h1>Contact Form</h1>
+      <form onSubmit={handleSubmit}>
+        <p>Name:</p>
+        <input type="text" value={name} onChange={event => {setName(event.target.value)}}></input>
+        <p>Phone:</p>
+        <input type="tel" pattern="^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$" value={phone} onChange={event => setPhone(event.target.value)}></input>
+        <p>Email:</p>
+        <input type="email" value={email} onChange={event => setEmail(event.target.value)}></input>
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+};
+
